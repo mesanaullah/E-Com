4a191ef397ab7655a21c8ce5d8d50d14c1b9166d
mesanaullah <mdsanaullah1227@gmail.com>
2025-07-21 23:31:20 +0530

    card page added

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -54,10 +54,19 @@ const ShopContextProvider = (Props) => {
         return totalCount;
     }
 
+    const updateQuantity = async (itemId, size, quantity) => {
+
+        let cartData = structuredClone (cartItems);
+
+        cartData[itemId][size] = quantity;
+        
+        setCartItems(cartData);
+    }
+
     const value = {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
-        cartItems, addToCart, getCartCount
+        cartItems, addToCart, getCartCount, updateQuantity
     }
 
     return (
